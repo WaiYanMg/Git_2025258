c118d64c09ce231f21957b3e22ce6984073a212b
WaiYanMg <waiyanmgw@gmail.com>
2025-05-01 09:40:40 +0100

    First Commit - Empty Project

--- src/git_2025258/Git_2025258.java
+++ src/git_2025258/Git_2025258.java
@@ -15,6 +15,7 @@ public class Git_2025258 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Wai Yan Maung - 2025258");
     }
     
 }
